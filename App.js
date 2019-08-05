e56c994da4713e461306caa827cce719b306dc7a
yi kyu seok <kyuseok.y@gamil.com>
2019-08-05 19:31:14 +0900

    add asset handler hadnler error

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
 import React from 'react';
+import { AppLoading, Font, Asset } from "expo";
+import { Ionicons } from "@expo/vector-icons";
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//     </View>
+//   );
+// }
+
+export default class App extends React.Component {
+  state = {
+    loaded: false
+  };
+
+  handleError = () => console.log(error);
+
+  handleLoaded = () => this.setState({ loaded: true });
+
+  loadAssets = async() => {
+    await Font.loadAsync({
+      ...Ionicons.font
+    });
+    // await Asset.loadAsync([
+    //
+    // ]);
+  };
+
+  render() {
+    const { loaded } = this.state;
+    if (loaded) {
+      return (
+          <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+          </View>
+      );
+    } else {
+      return (<AppLoading
+          startAsync={this.loadAssets}
+          onError={this.handleError}
+          onFinish={this.handleLoaded}/>
+      );
+    }
+  }
 }
 
 const styles = StyleSheet.create({
